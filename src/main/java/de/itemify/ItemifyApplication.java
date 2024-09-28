020202cc5a2885c052b0ec5658c0ef3415c36a7c
jkueck <jkueck@eos-uptrade.de>
2024-09-28 19:43:52 +0200

    initial commit

---
+++ src/main/java/de/itemify/ItemifyApplication.java
@@ -0,0 +1,13 @@
+package de.itemify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItemifyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItemifyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/itemify/ItemifyApplicationTests.java
@@ -0,0 +1,13 @@
+package de.itemify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItemifyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
